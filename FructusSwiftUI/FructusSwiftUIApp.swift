7278d70a4888063894614ad05357d2c71ec031e4
rustamkeneev <dr.rustamkeneev@gmail.com>
2021-07-17 21:05:40 +0600

    Initial Commit

---
+++ FructusSwiftUI/FructusSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusSwiftUIApp.swift
+//  FructusSwiftUI
+//
+//  Created by Rustam Keneev on 7/17/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
