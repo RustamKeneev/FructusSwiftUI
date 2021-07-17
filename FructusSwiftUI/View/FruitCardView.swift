acb46edb9d6d4ababf4e064f4dc980bd0a19c325
rustamkeneev <dr.rustamkeneev@gmail.com>
2021-07-17 23:19:40 +0600

    setting ZSTACK (frame, background and more)

--- FructusSwiftUI/View/FruitCardView.swift
+++ FructusSwiftUI/View/FruitCardView.swift
@@ -12,19 +12,22 @@ struct FruitCardView: View {
     
     //MARK: - BODY
     var body: some View {
-        VStack(spacing: 20) {
-            // FRUIT: IMAGE
-            Image("blueberry")
-                .resizable()
-                .scaledToFit()
-                .shadow(color: Color(red: 0, green: 0, blue: 0,opacity: 0.15), radius: 8, x: 6, y: 8)
-            // FRUIT: TITLE
-            Text("BlueBerry")
-            // FRUIT: HEADLINE
-            // BUTTON: START
-
-
-        } //: VSTACK
+        ZStack {
+            VStack(spacing: 20) {
+                // FRUIT: IMAGE
+                Image("blueberry")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: Color(red: 0, green: 0, blue: 0,opacity: 0.15), radius: 8, x: 6, y: 8)
+                // FRUIT: TITLE
+                Text("BlueBerry")
+                // FRUIT: HEADLINE
+                // BUTTON: START
+            } //: VSTACK
+        } //: ZSTACK
+        .frame(minWidth: 0,  maxWidth: .infinity, minHeight: 0, maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueBerryLight"), Color("ColorBlueBerryDark")]), startPoint: .top, endPoint: .bottom))
+        .cornerRadius(20)
     }
 }
 
