fbe14db8157b6a088367fb3bf4f882e6e4df1891
rustamkeneev <dr.rustamkeneev@gmail.com>
2021-07-18 13:07:24 +0600

    added fruit row view

---
+++ FructusSwiftUI/View/FruitRowView.swift
@@ -0,0 +1,27 @@
+//
+//  FruitRowView.swift
+//  FructusSwiftUI
+//
+//  Created by Rustam Keneev on 7/18/21.
+//
+
+import SwiftUI
+
+struct FruitRowView: View {
+    // MARK: - PROPERTIES
+
+    
+    // MARK: - BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: PREVIEW
+struct FruitRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitRowView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
